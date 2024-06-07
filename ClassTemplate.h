e2f5af8472e6812853008f4ad07d772324be9a9d
Kouta623 <k023g1021@g.neec.ac.jp>
2024-06-07 15:51:49 +0900

    確認課題（修正）

--- ClassTemplate.h
+++ ClassTemplate.h
@@ -3,19 +3,19 @@ template<typename T1, typename T2>
 class ClassTemplate
 {
 public:
-	T1 number1;
-	T2 number2;
+	T1 num1;
+	T2 num2;
 
 
-	ClassTemplate(T1 num1,T2 num2):number1(num1),number2(num2){}
+	ClassTemplate(T1 num1,T2 num2):num1(num1),num2(num2){}
 
 	T1 Size() {
-		if (number1<number2)
+		if (num1<num2)
 		{
-			return number1;
+			return num1;
 		}
 		else {
-			return number2;
+			return num2;
 		}
 	}
 
--- main.cpp
+++ main.cpp
@@ -5,12 +5,24 @@
 int main() {
 
 	ClassTemplate<int, int> result1(12, 23);
+
 	ClassTemplate<float, float> result2(1.5f, 0.5f);
+
 	ClassTemplate<double, double> result3(1.2, 5.4);
+
+	ClassTemplate<float,int> result4(12.8f, 13);
+
+	ClassTemplate<double,int> result5(0.5, 1);
+
+	ClassTemplate<float, double> result6(2.3f, 2.5);
 
 	printf("小さい値: %d\n", result1.Size());
 	printf("小さい値: %f\n", result2.Size()); 
 	printf("小さい値: %lf\n", result3.Size());
+	printf("小さい値: %f\n", result4.Size());
+	printf("小さい値: %lf\n", result5.Size());
+	printf("小さい値: %lf\n", result6.Size());
+
 
 	return 0;
 }
